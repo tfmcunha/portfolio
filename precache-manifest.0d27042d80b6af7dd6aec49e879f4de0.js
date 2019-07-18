70f954642b994ca21481b88ddf046b0f53e65aeb
Tiago Cunha <tcunha_lp@hotmail.com>
2019-07-18 12:19:08 +0100

    Updates

--- precache-manifest.0d27042d80b6af7dd6aec49e879f4de0.js
+++ precache-manifest.0d27042d80b6af7dd6aec49e879f4de0.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4cc433182bcf2de5d1b6d451e5e2cea5",
+    "revision": "aa55f67d407266f2922896d245d84ea8",
     "url": "/portfolio/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portfolio/static/css/2.22a7d4ef.chunk.css"
   },
   {
-    "revision": "830b9caed1892d47e1e6",
-    "url": "/portfolio/static/css/main.2618a3ca.chunk.css"
+    "revision": "4f9f28264f6986883789",
+    "url": "/portfolio/static/css/main.62d29c93.chunk.css"
   },
   {
     "revision": "eceed65f0dd6b12a5e50",
     "url": "/portfolio/static/js/2.a16c8998.chunk.js"
   },
   {
-    "revision": "830b9caed1892d47e1e6",
-    "url": "/portfolio/static/js/main.5b9b1540.chunk.js"
+    "revision": "4f9f28264f6986883789",
+    "url": "/portfolio/static/js/main.0a52ccde.chunk.js"
   },
   {
     "revision": "53ebc025fca807d4dbea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8cb77c3d2f8b6a7cda92c12f98b64576.js"
+  "/portfolio/precache-manifest.0d27042d80b6af7dd6aec49e879f4de0.js"
 );
 
 self.addEventListener('message', (event) => {
